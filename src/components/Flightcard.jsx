2cc80860e606925f38506d0690325787dd4df81d
Innes Ireland <innesmireland@gmail.com>
2022-11-04 12:44:23 +0000

    whatever

---
+++ src/components/Flightcard.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import { useEffect, useState } from 'react'
+
+const outboundUrl = "https://api.skypicker.com/flights?fly_from=PRG&fly_to=VLC&partner={data4youcbp202106}"
+const returnUrl = "https://api.skypicker.com/flights?fly_from=VLC&fly_to=PRG&partner={data4youcbp202106}"
+
+
+export default Flightcard(){
+
+}
