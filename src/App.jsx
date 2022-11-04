145c8f65b932574f9958ebf1c634a66bed16e374
Briana Dodgen <brianadodgen@gmail.com>
2022-11-04 10:00:58 +0100

    successfully fetching shit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const getApiUrl = (flyFrom, flyTo, dateFrom, dateTo, resultLimit) =>
+  `https://api.skypicker.com/flights?fly_from=${flyFrom}&fly_to=${flyTo}&date_from=${dateFrom}&date_to=${dateTo}&limit=${resultLimit}&partner=data4youcbp202106`;
+
+const App = () => {
+  const [flightData, setFlightData] = useState("");
+
+  // // for setting and holding whatever we search for
+  // const [searchQuery, setSearchQuery] = useState("");
+  // // I assume for setting and holding whatever we get back
+  // const [searchResult, setSearchResult] = useState("");
+  // // fuck if I know. A boolean that doesn't make sense.
+  // // apparently just a think for a "wait for this" message
+  // // to the user 
+  const [isLoading, setIsLoading] = useState(false);
+
+  // tells the code what you want it to react to?? using the 
+  // array at the bottom that you sort of understand
+  useEffect(() => {
+    // gets the shit from the API and for some reason we put it
+    // in the useEffect, which we've never done before
+    const loadData = async () => {
+      // fucking why though?
+      setIsLoading(true);
+
+      // the usual except it calls that function that builds the url 
+      // with our search data
+      const url = getApiUrl('PRG', 'VLC', '11/11/2022', '20/11/2022', 10);
+      const res = await fetch(url);
+      const data = await res.json();
+
+      // this appears to be pulling the search data from some kind 
+      // of magic that generated it
+      setFlightData(data);
+      console.log(data);
+
+      setIsLoading(false);
+    };
+
+    loadData();
+
+    // only loads data if something has been searched (need searchQuery in [] below)
+    // if (searchQuery) {
+    //   loadData();
+    // }
+  }, []);
+
+  // makes the actual searchy thing in its entirety
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
     </div>
   );
-}
+};
 
 export default App;
